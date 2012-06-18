76e59bcf01cd269c6a26c342191c79509fce8d08
Victor Savkin <vic.savkin@gmail.com>
2012-06-17 21:06:50 -0400

    adds unit_test_helper to test rails free classes and model_test_helper to test active record classes

---
+++ spec/model_spec_helper.rb
@@ -0,0 +1,22 @@
+require 'unit_spec_helper'
+require 'active_record'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
+
+db_yml_file = File.expand_path('config/database.yml')
+db_config = YAML.load_file(db_yml_file)
+ActiveRecord::Base.establish_connection(db_config['test'])
---
+++ spec/unit_spec_helper.rb
@@ -0,0 +1,8 @@
+rails_root = File.expand_path('../../', __FILE__)
+$LOAD_PATH.unshift(rails_root) unless $LOAD_PATH.include?(rails_root)
+
+require 'active_support/dependencies'
+autoload_paths = ActiveSupport::Dependencies.autoload_paths
+%w(app/models).each do |path|
+  autoload_paths.push(path) unless autoload_paths.include?(path)
+end
