738bae75f47fbe86ae2b6cd0922be890c1abda25
Victor Savkin <vic.savkin@gmail.com>
2012-06-21 08:19:13 -0400

    changes to the ui to make acceptance tests pass

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -7,7 +7,12 @@ class AuctionsController < ApplicationController
 
   def create
     auction_params = AuctionParams.new(params[:auction_params])
-    auction = CreateAuction.create current_user, auction_params
-    redirect_to auction_path(auction)
+    result = CreateAuction.create current_user, auction_params
+
+    if result.has_key? :errors
+      render :status  => :unprocessable_entity, :json => result
+    else
+      render :json => {auction_path: auction_path(result[:auction])}
+    end
   end
 end
