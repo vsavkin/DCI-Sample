2959eb42d79323d2f3b3baa2b8d8ef4c3d444a23
Victor Savkin <vic.savkin@gmail.com>
2012-06-21 08:17:12 -0400

    adds acceptance tests for error handling

--- spec/requests/start_auctions_spec.rb
+++ spec/requests/start_auctions_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "Creating Auction" do
+feature "Creating Auction", js: true do
   background do
     User.create(name: "Sam the Seller")
   end
@@ -10,9 +10,20 @@ feature "Creating Auction" do
 
     fill_in "Item name", with: "Stamps"
     fill_in "Item description", with: "Collection of stamps"
-    fill_in "Price", with: "200"
+    fill_in "Buy it now price", with: "200"
     click_button "Create"
 
     page.should have_content("Successfully created auction.")
+  end
+
+  scenario "Showing errors when cannot create an auction" do
+    visit new_auction_path
+
+    fill_in "Item name", with: ""
+    fill_in "Item description", with: "Collection of stamps"
+    fill_in "Buy it now price", with: "200"
+    click_button "Create"
+
+    page.should have_content("Name can't be blank")
   end
 end
