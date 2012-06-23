ba114a7efef87bf7071fc68a97a67bb0b3b73ab2
Victor Savkin <vic.savkin@gmail.com>
2012-06-23 15:46:49 -0400

    extracts database cleaner configuration into a separate file

--- spec/model_spec_helper.rb
+++ spec/model_spec_helper.rb
@@ -1,22 +1,7 @@
 require 'unit_spec_helper'
 require 'active_record'
 require 'virtus'
-require 'database_cleaner'
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-end
+require 'database_cleaner_config'
 
 db_yml_file = File.expand_path('config/database.yml')
 db_config = YAML.load_file(db_yml_file)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/autorun'
 
 require 'capybara/rspec'
 require 'capybara/rails'
+require 'database_cleaner_config'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -30,20 +31,8 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_base_class_for_anonymous_controllers = true
 end
