ee4ab57890d8177e11f2abb848de4b94c0629cfd
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-07-07 12:06:39 -0300

    renames buyer to winner and buy_item to win

--- spec/models/bidding_spec.rb
+++ spec/models/bidding_spec.rb
@@ -2,18 +2,18 @@ require 'model_spec_helper'
 
 describe Bidding do
   let(:auction){double("Auction")}
-  let(:buyer){double("Buyer")}
-  subject{ described_class.new buyer, auction }
+  let(:winner){double("Winner")}
+  subject{ described_class.new winner, auction }
 
   context "successful creation" do
     it "makes a bid" do
-      described_class.should_receive(:new).with(buyer, auction).and_return(bidding = mock("bidding"))
+      described_class.should_receive(:new).with(winner, auction).and_return(bidding = mock("bidding"))
       bidding.should_receive(:make_bid)
-      described_class.make_bid buyer, auction
+      described_class.make_bid winner, auction
     end
 
     it "buys an item" do
-      buyer.should_receive(:buy_item).and_return auction
+      winner.should_receive(:win).and_return auction
       subject.make_bid.should == {auction:  auction}
     end
 
@@ -25,7 +25,7 @@ describe Bidding do
 
   context "error handling" do
     it "returns errors when cannot make a bid" do
-      auction.should_receive(:assign_buyer).with(buyer).and_raise(InvalidRecordException.new([:error]))
+      auction.should_receive(:assign_buyer).with(winner).and_raise(InvalidRecordException.new([:error]))
       subject.make_bid.should == {errors:  [:error]}
      end
   end
