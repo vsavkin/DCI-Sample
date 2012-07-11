85e8df005f371aa3b4eb2dc6a980e22f633d7bcb
Victor Savkin <vic.savkin@gmail.com>
2012-07-10 23:17:35 -0400

    removes address caching

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,14 @@ class User < ActiveRecord::Base
   attr_accessible :name
 
   def address
-    @address ||= Address.new(address_country, address_city, address_street, address_postal_code)
+    Address.new(address_country, address_city, address_street, address_postal_code)
   end
 
-  def address=(address)
+  def address= address
     self[:address_country] = address.country
     self[:address_city] = address.city
     self[:address_street] = address.street
     self[:address_postal_code] = address.postal_code
-
-    @address = address
   end
 end
 
