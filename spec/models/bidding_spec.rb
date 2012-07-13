f547f15293c444728f2991d9e72807afa533a5cb
Victor Savkin <vic.savkin@gmail.com>
2012-07-12 20:29:03 -0400

    refactors bidding

--- spec/models/bidding_spec.rb
+++ spec/models/bidding_spec.rb
@@ -2,31 +2,25 @@ require 'model_spec_helper'
 
 describe Bidding do
   let(:auction){double("Auction")}
-  let(:winner){double("Winner")}
-  subject{ described_class.new winner, auction }
+  let(:bidder){User.new}
 
-  context "successful creation" do
-    it "makes a bid" do
-      described_class.should_receive(:new).with(winner, auction).and_return(bidding = mock("bidding"))
-      bidding.should_receive(:make_bid)
-      described_class.make_bid winner, auction
-    end
-
-    it "buys an item" do
-      winner.should_receive(:win).and_return auction
-      subject.make_bid.should == {auction:  auction}
-    end
-
-    it "closes the auction" do
-      auction.should_receive(:close_auction)
-      subject.make_bid
+  context "buying" do
+    it "should assign a winner" do
+      auction.should_receive(:assign_winner).with(bidder)
+      make_bid
     end
   end
 
   context "error handling" do
     it "returns errors when cannot make a bid" do
-      auction.should_receive(:assign_buyer).with(winner).and_raise(InvalidRecordException.new([:error]))
-      subject.make_bid.should == {errors:  [:error]}
+      auction.should_receive(:assign_winner).with(bidder).and_raise(InvalidRecordException.new([:error]))
+      make_bid.should == {errors:  [:error]}
      end
+  end
+
+  private
+
+  def make_bid
+    Bidding.new(bidder, auction).make_bid
   end
 end
