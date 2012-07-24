243dcf68bc493c5be15a515dfad2a3d1b8480a39
Victor Savkin <vic.savkin@gmail.com>
2012-07-23 22:13:19 -0400

    adds tests to auction params

--- app/models/auction_params.rb
+++ app/models/auction_params.rb
@@ -7,20 +7,34 @@ class AuctionParams
   attribute :buy_it_now_price, Decimal
   attribute :end_date, DateTime
 
-  def initialize params = nil
-    if params
-      end_date = DateTime.new(params["end_date(1i)"].to_i, params["end_date(2i)"].to_i, params["end_date(3i)"].to_i, params["end_date(4i)"].to_i, params["end_date(5i)"].to_i)
-      super params.merge({"end_date" => end_date})
-    else
-      super
-    end
+  def initialize params
+    super build_end_date(params)
   end
 
   def self.empty
-    AuctionParams.new
+    AuctionParams.new({})
   end
 
   def == other
     attributes == other.attributes
+  end
+
+  private
+
+  def build_end_date params
+    return unless has_end_date? params
+
+    year = params["end_date(1i)"].to_i
+    month = params["end_date(2i)"].to_i
+    day = params["end_date(3i)"].to_i
+    hour = params["end_date(4i)"].to_i
+    minute = params["end_date(5i)"].to_i
+
+    date = DateTime.new(year, month, day, hour, minute)
+    params.merge(end_date: date)
+  end
+
+  def has_end_date? params
+    params.has_key? "end_date(1i)"
   end
 end
