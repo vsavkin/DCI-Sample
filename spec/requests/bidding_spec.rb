4c62b23c0904776940c26d2f8bbdab55869e532a
Victor Savkin <vic.savkin@gmail.com>
2012-07-27 07:51:28 -0400

    two specs testing bidding

--- spec/requests/bidding_spec.rb
+++ spec/requests/bidding_spec.rb
@@ -13,24 +13,36 @@ feature "Buying an Item", js: true do
     Auction.create!(item: item, seller: seller, buy_it_now_price: 10, status: Auction::STARTED)
   }
 
-  scenario "Setting the buyer" do
-    user = User.create!(name: "Bob", email: "mail1@email.com", password: "123456")
-    do_login! user
+  let(:bidder){
+    User.create!(name: "Bob", email: "mail1@email.com", password: "123456")
+  }
 
+  before :each do
+    do_login! bidder
+  end
+
+  scenario "Setting the buyer" do
     visit auction_path(auction)
     click_link "Buy It Now!"
 
     find("#winner").text.should == "Bob"
   end
 
-  #scenario "Making a bid" do
-  #  user = User.create!(name: "Bob", email: "mail1@email.com", password: "123456")
-  #  do_login! user
-  #
-  #  visit auction_path(auction)
-  #
-  #  click_link "Buy It Now!"
-  #
-  #  find("#winner").text.should == "Bob"
-  #end
+  scenario "Making a bid" do
+    visit auction_path(auction)
+    fill_in "bid_params_amount", with: "5"
+
+    click_button "Bid"
+
+    page.should have_content("Your bid is accepted")
+  end
+
+  scenario "Making an invalid bi" do
+    visit auction_path(auction)
+    fill_in "bid_params_amount", with: "INVALID"
+
+    click_button "Bid"
+
+    page.should have_content("is not a number")
+  end
 end
