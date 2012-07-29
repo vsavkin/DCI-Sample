f8b01014dac94a3eb7a474396444b5f25bc156cd
Victor Savkin <vic.savkin@gmail.com>
2012-07-28 21:05:03 -0400

    makes closing expired auctions responsible for assigning a winner if there was a successful bid

--- app/contexts/closing_expired_auctions.rb
+++ app/contexts/closing_expired_auctions.rb
@@ -1,32 +1,49 @@
 class ClosingExpiredAuctions
   include Context
 
-  attr_reader :auctions
-
   def self.close auctions
     ClosingExpiredAuctions.new(auctions).close
   end
 
+  attr_reader :expirable_auctions
+
   def initialize auctions
-    auctions.map{|auction| auction.extend Expirable}
-    @auctions = auctions
+    @expirable_auctions = auctions.map{|auction| auction.extend Expirable}
   end
 
   def close
     in_context do
-      auctions.each do |auction|
-        auction.closes_when_expired
+      expirable_auctions.each do |auction|
+        auction.close_if_expired
       end
     end
   end
 
   module Expirable
-    def expired?
-      end_date < DateTime.current
+    def close_if_expired
+      return unless expired?
+
+      if winning_bid?
+        close_with_winner
+      else
+        close_without_winner
+      end
     end
 
-    def closes_when_expired
-      update_attribute(:status, CLOSED) if expired?
+    def close_with_winner
+      assign_winner last_bid.user
+    end
+
+    def close_without_winner
+      close
+    end
+
+    def winning_bid?
+      last_bid.present?
+    end
+
+    def expired?
+      end_date < DateTime.current and status == Auction::STARTED
     end
   end
 end
--- spec/models/closing_expired_auctions_spec.rb
+++ spec/models/closing_expired_auctions_spec.rb
@@ -1,10 +1,37 @@
 require 'model_spec_helper'
 
 describe ClosingExpiredAuctions do
-  let(:auction) { mock('Auction') }
+  context "expired auction" do
+    let(:bidder){stub}
+    let(:bid){stub(user: bidder)}
 
-  it "expires auctions closing each one" do
-    auction.should_receive(:closes_when_expired)
-    described_class.close [auction]
+    it "closes an auction if its end date is in the past and it's started" do
+      auction = expired_auction last_bid: nil
+      auction.should_receive(:close)
+      ClosingExpiredAuctions.close [auction]
+    end
+
+    it "assigns a winner when auction has a bid" do
+      auction = expired_auction last_bid: bid
+      auction.should_receive(:assign_winner).with(bidder)
+      ClosingExpiredAuctions.close [auction]
+    end
+  end
+
+  context "not expired auction" do
+    it "doesn't close an auction if its end date is in the future" do
+      auction = stub(end_date: DateTime.current + 1.day, status: Auction::STARTED)
+      ClosingExpiredAuctions.close [auction]
+    end
+
+    it "doesn't close an auction if it's not started" do
+      auction = stub(end_date: DateTime.current - 1.day, status: Auction::PENDING)
+      ClosingExpiredAuctions.close [auction]
+    end
+  end
+
+  private
+  def expired_auction attrs = {}
+    mock({end_date: DateTime.current - 1.day, status: Auction::STARTED}.merge(attrs))
   end
 end
