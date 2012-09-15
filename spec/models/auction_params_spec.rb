9dbd83f2443c9cebce89248b7861b50eade10c05
Victor Savkin <vic.savkin@gmail.com>
2012-09-15 14:01:57 -0400

    fixes a bug when not string end date is provided

--- spec/models/auction_params_spec.rb
+++ spec/models/auction_params_spec.rb
@@ -7,13 +7,20 @@ describe AuctionParams do
      "end_date(4i)" => 9, "end_date(5i)" => 8}
   end
 
-  it "should build end date from array of strings" do
+  it "builds end date from array of strings" do
     params = AuctionParams.new(params_with_string_date)
     params.end_date.should == DateTime.new(2001,11,10,9,8)
   end
 
-  it "should not set date end when no input" do
-    params = AuctionParams.new({})
-    params.end_date.should be_nil
+  it "uses real date when provided" do
+    date = DateTime.new(2099,11,10,9,8)
+    params = AuctionParams.new(end_date: date)
+    params.end_date.should == date
+  end
+
+  it "prefers real date" do
+    another_date = DateTime.new(2099,11,10,9,8)
+    params = AuctionParams.new(params_with_string_date.merge(end_date: another_date))
+    params.end_date.should == another_date
   end
 end
